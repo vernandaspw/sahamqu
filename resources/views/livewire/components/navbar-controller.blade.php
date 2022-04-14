6060572c4611191039427e2e82e0a2aa50191353
vernandaspw <vernandaspw@gmail.com>
2022-04-14 23:55:34 +0700

    d

--- resources/views/livewire/components/navbar-controller.blade.php
+++ resources/views/livewire/components/navbar-controller.blade.php
@@ -1,35 +1,35 @@
 <div>
-    <nav class="nav nav-pills flex-column fixed-bottom bg-white shadow">
-        <ul class="nav d-flex justify-content-between mx-3">
-            <li class="nav-item">
-                <a class="nav-link @if (request()->is('beranda'))
+
+    <ul class="nav d-flex justify-content-around mx-1 fixed-bottom bg-white shadow-lg">
+        <li class="nav-item">
+            <a class="nav-link @if (request()->is('beranda'))
                     text-primary
                     @else
                     text-dark
                 @endif" aria-current="page" href="{{ url('beranda') }}">Beranda</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link  @if (request()->is('broker'))
+        </li>
+        <li class="nav-item">
+            <a class="nav-link  @if (request()->is('broker'))
                     text-primary
                     @else
                     text-dark
                 @endif" href="{{ url('broker') }}">Broker</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link  @if (request()->is('perusahaan'))
+        </li>
+        <li class="nav-item">
+            <a class="nav-link  @if (request()->is('perusahaan'))
                     text-primary
                     @else
                     text-dark
                 @endif" href="{{ url('perusahaan') }}">Perusahaan</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link  @if (request()->is('akun'))
+        </li>
+        <li class="nav-item">
+            <a class="nav-link  @if (request()->is('akun'))
                     text-primary
                     @else
                     text-dark
                 @endif" href="{{ url('akun') }}">Akun</a>
-            </li>
-        </ul>
-    </nav>
+        </li>
+    </ul>
+
 
 </div>
