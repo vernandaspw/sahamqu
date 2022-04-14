37541168901886df7ce6f8975de12719f627c970
vernandaspw <vernandaspw@gmail.com>
2022-04-15 00:39:17 +0700

    d

--- resources/views/livewire/components/navbar-controller.blade.php
+++ resources/views/livewire/components/navbar-controller.blade.php
@@ -1,34 +1,54 @@
 <div>
+    {{-- <nav class="navbar navbar-expand fixed-bottom navbar-dark bg-primary shadow-lg">
+        <div class="container-fluid d-flex justify-content-evenly">
+            <ul class="navbar-nav m-auto d-flex justify-content-evenly flex-nowrap">
+                <li class="nav-item">
+                    <a class="nav-link @if (request()->is('beranda'))
+                        active
+                    @endif" href="{{ url('beranda') }}">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link @if (request()->is('broker'))
+                        active
+                    @endif" href="{{ url('broker') }}">Broker</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link @if (request()->is('perusahaan'))
+                        active
+                    @endif" href="{{ url('perusahaan') }}">Perusahaan</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link @if (request()->is('akun'))
+                        active
+                    @endif" href="{{ url('akun') }}">Akun</a>
+                </li>
 
-    <ul class="nav d-flex justify-content-around mx-1 fixed-bottom bg-white shadow-lg">
+            </ul>
+        </div>
+    </nav> --}}
+
+    <ul class="nav  py-1 justify-content-evenly fixed-bottom bg-primary flex-nowrap">
         <li class="nav-item">
-            <a class="nav-link @if (request()->is('beranda'))
-                    text-primary
-                    @else
-                    text-dark
-                @endif" aria-current="page" href="{{ url('beranda') }}">Beranda</a>
+            <a class="nav-link  fw-light text-white  @if (request()->is('beranda'))
+                fw-bolder
+            @endif" href="#" style="font-size: 15px">Beranda</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link  @if (request()->is('broker'))
-                    text-primary
-                    @else
-                    text-dark
-                @endif" href="{{ url('broker') }}">Broker</a>
+            <a class="nav-link fw-light text-white @if (request()->is('broker'))
+                fw-bolder
+            @endif" href="#" style="font-size: 15px">Broker</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link  @if (request()->is('perusahaan'))
-                    text-primary
-                    @else
-                    text-dark
-                @endif" href="{{ url('perusahaan') }}">Perusahaan</a>
+            <a class="nav-link fw-light text-white @if (request()->is('perusahaan'))
+                    fw-bolder
+            @endif" href="#" style="font-size: 15px">Perusahaan</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link  @if (request()->is('akun'))
-                    text-primary
-                    @else
-                    text-dark
-                @endif" href="{{ url('akun') }}">Akun</a>
+            <a class="nav-link fw-light text-white @if (request()->is('akun'))
+                    fw-bolder
+            @endif" href="#" style="font-size: 15px">Akun</a>
         </li>
+
     </ul>
 
 
