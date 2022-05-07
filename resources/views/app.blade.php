e59b005ba20a7b881ffd2bb62eaa6d98af0bc8d9
vernandaspw <vernandaspw@gmail.com>
2022-05-07 09:26:17 +0700

    d

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -50,7 +50,7 @@
     <script src="{{ asset('sw.js') }}"></script>
     <script>
         if (!navigator.serviceWorker.controller) {
-        navigator.serviceWorker.register("sw.js").then(function (reg) {
+        navigator.serviceWorker.register({{ asset('sw.js') }}).then(function (reg) {
             console.log("Service worker has been registered for scope: " + reg.scope);
         });
     }
