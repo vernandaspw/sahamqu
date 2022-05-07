8e99f98bef8ed1026ccf6d42352120f9e1f75714
vernandaspw <vernandaspw@gmail.com>
2022-05-07 09:00:41 +0700

    d

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="shortcut icon" href="{{ asset('logo.png') }}" type="image/x-icon">
     {{-- PWA --}}
     <meta name="theme-color" content="#6777ef" />
