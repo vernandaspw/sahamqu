33ec0c5fbf3e630d5ba2e244077667869731d0d2
vernandaspw <vernandaspw@gmail.com>
2022-05-13 19:26:04 +0700

    ddd

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,60 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="shortcut icon" href="{{ asset('logo.png') }}" type="image/x-icon">
+
     {{-- PWA --}}
-
-    <!-- Web Application Manifest -->
-    {{--
-    <link rel="manifest" href="{{asset('manifest.json')}}"> --}}
-    <!-- Chrome for Android theme color -->
-    <meta name="theme-color" content="#000000">
-
-    <!-- Add to homescreen for Chrome on Android -->
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="application-name" content="PWA">
-    <link rel="icon" sizes="512x512" href="{{ asset('logo.png') }}">
-
-    <!-- Add to homescreen for Safari on iOS -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta name="apple-mobile-web-app-title" content="PWA">
-    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
-
-    <link href="/images/icons/splash-640x1136.png"
-        media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
-    <link href="/images/icons/splash-750x1334.png"
-        media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
-    <link href="/images/icons/splash-1242x2208.png"
-        media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
-        rel="apple-touch-startup-image" />
-    <link href="/images/icons/splash-1125x2436.png"
-        media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
-        rel="apple-touch-startup-image" />
-    <link href="/images/icons/splash-828x1792.png"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
-    <link href="/images/icons/splash-1242x2688.png"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
-        rel="apple-touch-startup-image" />
-    <link href="/images/icons/splash-1536x2048.png"
-        media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
-    <link href="/images/icons/splash-1668x2224.png"
-        media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
-    <link href="/images/icons/splash-1668x2388.png"
-        media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
-    <link href="/images/icons/splash-2048x2732.png"
-        media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
-
-    <!-- Tile for Win8 -->
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="{{ asset('logo.png') }}">
-    {{-- end pwa --}}
+    <!-- PWA  -->
+    <meta name="theme-color" content="#00A2FF" />
+    <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
 
 
 
@@ -81,21 +33,8 @@
     @livewireStyles
 
 
-    <script type="text/javascript">
-        // Initialize the service worker
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register({{ asset('serviceworker.js') }}, {
-                scope: '.'
-            }).then(function (registration) {
-                // Registration was successful
-                console.log('Laravel PWA: ServiceWorker registration successful with scope: ', registration.scope);
-            }, function (err) {
-                // registration failed :(
-                console.log('Laravel PWA: ServiceWorker registration failed: ', err);
-            });
-        }
-    </script>
-    @laravelPWA
+
+
 </head>
 
 <body class="antialiased">
@@ -113,6 +52,14 @@
         integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
     </script>
 
+    <script src="{{ asset('/sw.js') }}"></script>
+    <script>
+        if (!navigator.serviceWorker.controller) {
+        navigator.serviceWorker.register("/sw.js").then(function (reg) {
+            console.log("Service worker has been registered for scope: " + reg.scope);
+        });
+    }
+    </script>
 
 
 </body>
--- resources/views/offline.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('app')
-
-@section('content')
-
-<h1>You are currently not connected to any networks.</h1>
-
-@endsection
